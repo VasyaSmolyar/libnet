0fc78571c86e356862d171d4e73d6581898d140c
Василий Смоляр <smolyar4@gmail.com>
2023-09-10 13:33:00 +0300

    feat: mysql package

---
+++ pkg/db/mysql.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/spf13/viper"
+)
+
+func Init(cfg *viper.Viper) (*DBConnect, error) {
+	conn := &DBConnect{config: cfg}
+	db, err := conn.connect()
+	if err != nil {
+		return nil, err
+	}
+	conn.Conn = db
+	return conn, nil
+}
+
+type DBConnect struct {
+	config *viper.Viper
+	Conn   *sql.DB
+}
+
+func (db *DBConnect) connect() (*sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@/%s",
+		db.config.GetString("db.user"),
+		db.config.GetString("db.pass"),
+		db.config.GetString("db.name"),
+	)
+
+	conn, err := sql.Open(db.config.GetString("db.driver"), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.SetConnMaxLifetime(time.Minute * time.Duration(db.config.GetInt("db.maxLifetime")))
+	conn.SetMaxOpenConns(db.config.GetInt("db.maxOpenConns"))
+	conn.SetMaxIdleConns(db.config.GetInt("db.maxIdleConns"))
+
+	return conn, nil
+}
